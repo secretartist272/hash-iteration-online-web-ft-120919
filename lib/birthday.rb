88a15006a5039156d76e5309f269f0e515f45b0d
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 02:40:04 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,3 +1,6 @@
+require 'pry'
+
+
 # birthday_kids = {
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
@@ -5,7 +8,20 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+   birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end 
 end
 
 
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    if age > 12 
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
+    if age < 12
+      puts "You are to old for this"
+    end
+  end
+end    
+     
