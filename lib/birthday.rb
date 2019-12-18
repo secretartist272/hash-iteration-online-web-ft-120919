4df9f2a25b68395e3d93f7306402cf94d2e172eb
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 02:42:21 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -20,7 +20,7 @@ def age_appropriate_birthday(birthday_kids)
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     end
     if age < 12
-      puts "You are to old for this"
+      puts "You are to old for this."
     end
   end
 end    
