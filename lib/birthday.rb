2ea6e14caf573aa76549b9f7bf5b6b2f52f57af8
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 02:49:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+birthday_kids = {
+  "Timmy" => 9, 
+  "Sarah" => 6, 
+  "Amanda" => 27
+}
 
 def happy_birthday(birthday_kids)
    birthday_kids.each do |name, age|
