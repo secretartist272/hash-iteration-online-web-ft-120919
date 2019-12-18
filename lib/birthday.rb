18c03d1be654239d40bcbcde2974a4fe3cd4126d
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 02:48:39 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,10 +16,10 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    if age >= 12 
+    if age > 12 
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     end
-    if age =< 12
+    if age < 12
       puts "You are to old for this."
     end
   end
